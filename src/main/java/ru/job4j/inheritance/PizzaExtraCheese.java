0143dba527b3e772d837b60b0282970757299d6c
Aleksey-1983 <0230595@mail.ru>
2023-04-29 23:35:29 +0300

    pom

--- src/main/java/ru/job4j/inheritance/PizzaExtraCheese.java
+++ src/main/java/ru/job4j/inheritance/PizzaExtraCheese.java
@@ -2,11 +2,6 @@ package ru.job4j.inheritance;
 
 public class PizzaExtraCheese extends Pizza {
     private static final String name = "extra cheese";
-
-    public PizzaExtraCheese(String name) {
-        super(name);
-    }
-
     public String name() {
         return name + super.name();
     }
--- src/main/java/ru/job4j/inheritance/PizzaExtraCheeseExtraTomato.java
+++ src/main/java/ru/job4j/inheritance/PizzaExtraCheeseExtraTomato.java
@@ -2,11 +2,6 @@ package ru.job4j.inheritance;
 
 public class PizzaExtraCheeseExtraTomato extends PizzaExtraCheese {
     private static final String name = "extra tomato";
-
-    public PizzaExtraCheeseExtraTomato(String name) {
-        super(name);
-    }
-
     public String name() {
         return name + super.name();
     }
